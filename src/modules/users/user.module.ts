f68b7a8d07d0b15848f05344a930f8379b16a131
Mohamed Abdo <mohamedabdoelatar@gmail.com>
2025-01-11 00:10:43 +0200

    services

--- src/modules/users/user.module.ts
+++ src/modules/users/user.module.ts
@@ -1,11 +1,12 @@
 import { Module } from "@nestjs/common";
 import { UsersController } from "./user.controller";
+import { UserService } from "./user.service";
 
 
 
 @Module({
     controllers: [UsersController],
-    providers: [],
+    providers: [UserService],
     exports: [],
     imports: []
 })
