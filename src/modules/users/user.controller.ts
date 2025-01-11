f16f17b55837695afb1c9de5c67ca21aa14af991
Mohamed Abdo <mohamedabdoelatar@gmail.com>
2025-01-11 03:12:34 +0200

    extracting data from req and starting pipes

--- src/modules/users/user.controller.ts
+++ src/modules/users/user.controller.ts
@@ -1,11 +1,25 @@
-import { Controller, Get } from "@nestjs/common";
+import { Body, Controller, Get, Param, ParseIntPipe, ParseUUIDPipe, Post, Query } from "@nestjs/common";
 import { UserService } from "./user.service";
 
 @Controller('users')
 export class UsersController {
     constructor(private userService: UserService) { }
     @Get()
-    getAllUsers() {
+    getAllUsers(@Body() body : any) {
+        console.log(body);
+        return this.userService.getAllUsers();
+    }
+
+    
+    @Get(":id")
+    getUserWithID(@Param("id" , ParseUUIDPipe) param : number) {
+        console.log(param);
+        return 'this.userService.getAllUsers()';
+    }
+
+    @Post()
+    testQuery(@Query() query : any) {
+        console.log(query);
         return this.userService.getAllUsers();
     }
 
